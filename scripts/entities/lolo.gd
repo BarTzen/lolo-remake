779ada82c0e6e7b7bd4c7f98f7fe008401b8c978
André Bartzen de Araújo <andrearaujo@sou.faccat.br>
2026-04-17 21:39:28 -0300

    hello world
    update - hello world

--- scripts/entities/lolo.gd
+++ scripts/entities/lolo.gd
@@ -14,19 +14,20 @@ var _moving     := false
 var _target_pos := Vector2.ZERO
 
 @onready var _sprite  := $AnimatedSprite2D as AnimatedSprite2D
-@onready var _tilemap := _find_tilemap()
+#@onready var _tilemap := _find_tilemap()
 
 
 func _ready() -> void:
 	_target_pos = position
 	_sprite.play("idle")
 
-
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if _moving:
-		_step(delta)
+		pass
+		#_step(delta)
 	else:
 		_read_input()
+	move_and_slide()
 
 
 # ── Movimento público — pode ser chamado de qualquer lugar ────────────────────
@@ -56,22 +57,22 @@ func _read_input() -> void:
 func _try_move(dir: Vector2i) -> void:
 	var next_tile := Vector2i(position / TILE_SIZE) + dir
 	_sprite.play(ANIM_FOR_DIR[dir])
-	if _tilemap and TileTypes.is_solid(_tilemap, next_tile):
-		return
+	#if _tilemap and TileTypes.is_solid(_tilemap, next_tile):
+		#return
 	_moving     = true
 	_target_pos = position + Vector2(dir) * TILE_SIZE
 
 
-func _step(delta: float) -> void:
-	position = position.move_toward(_target_pos, MOVE_SPEED * delta)
-	if position.is_equal_approx(_target_pos):
-		position = _target_pos
-		_moving  = false
-		_sprite.play("idle")
-
-
-func _find_tilemap() -> TileMapLayer:
-	var p := get_parent()
-	if p and p.get_parent():
-		return p.get_parent().get_node_or_null("TileMapLayer") as TileMapLayer
-	return null
+#func _step(delta: float) -> void:
+	#position = position.move_toward(_target_pos, MOVE_SPEED * delta)
+	#if position.is_equal_approx(_target_pos):
+		#position = _target_pos
+		#_moving  = false
+		#_sprite.play("idle")
+#
+#
+#func _find_tilemap() -> TileMapLayer:
+	#var p := get_parent()
+	#if p and p.get_parent():
+		#return p.get_parent().get_node_or_null("TileMapLayer") as TileMapLayer
+	#return null
